cc83ddb76fddb9f85a0540a1f0a589f498e505d4
evan <kirkilese@gmail.com>
2022-10-19 20:10:55 -0400

    Added images, fixed layout.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,6 +5,9 @@
  * 2022 the nobot space,
  */
 import s from './Layout.module.scss';
+import Image from 'next/image';
+import LHS_IMG from '../../../public/images/lhs.png';
+import RHS_IMG from '../../../public/images/rhs.png';
 
 type LayoutProps = {
   children?: React.ReactNode;
@@ -29,6 +32,28 @@ const Layout: React.FC<LayoutProps> = function Layout({ children }) {
         className={s.printing_block_tiny}
         style={{ bottom: '10px', right: '10px' }}
       />
+      <div className={s.desk_image_left}>
+        <div className={s.desk_image_container}>
+          <Image
+            src={LHS_IMG}
+            layout="fill"
+            objectFit={'contain'}
+            alt={'desk image left'}
+            placeholder={'blur'}
+          />
+        </div>
+      </div>
+      <div className={s.desk_image}>
+        <div className={s.desk_image_container}>
+          <Image
+            src={RHS_IMG}
+            layout="fill"
+            objectFit={'contain'}
+            alt={'desk image right'}
+            placeholder={'blur'}
+          />
+        </div>
+      </div>
       <div className={s.contents}>
         <div className={s.contents_outline}></div>
         <div className={s.printing_block}></div>
@@ -37,12 +62,13 @@ const Layout: React.FC<LayoutProps> = function Layout({ children }) {
           <div></div>
           <div className={s.inner_printing_block}></div>
         </div>
+        <div style={{ flex: 1 }}></div>
         <div className={s.content_container}>
           {children}
           <div className={s.top_scroll_gradient}></div>
           <div className={s.lower_scroll_gradient}></div>
         </div>
-        <div className={s.printing_row}>
+        <div className={s.printing_info_row}>
           <div>/index.html</div>
           <div>1</div>
           <div>(c) 2022</div>
